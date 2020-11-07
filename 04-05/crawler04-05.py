d76e73b5cdf7646e328533e4f7199031c0ca47b9
jhj614 <jhj614@hotmail.com>
2020-11-07 13:12:45 -0800

    Added crawler04-05.py

---
+++ 04-05/crawler04-05.py
@@ -0,0 +1,9 @@
+# 题⽬要求
+# 根据已给的⽹址分析⽹站特征，将最新发布的岗位第⼀⻚的标题例如（28297-（⾼级）
+# 海外战略分析经理）爬取到本地。
+# 使⽤技术点引导
+#   request s库
+#   json()
+#   for循环
+# 地址为： https://careers.tencent.com/tencentcareer/api/post/Query?timestamp=1585450579819&countryId=&cityId=&bgIds=&productId=&categoryId=&parentCategoryId=&attrId=&keyword=&pageIndex=1&pageSize=10&language=zh-cn&area=cn
+
